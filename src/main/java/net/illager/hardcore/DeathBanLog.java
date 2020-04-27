f0ea73eb93362a95affbb53f1f6809fd4db61d57
metanomial <benjamin@metanomial.com>
2020-04-27 11:47:38 -0700

    Add username method variations

--- src/main/java/net/illager/hardcore/DeathBanLog.java
+++ src/main/java/net/illager/hardcore/DeathBanLog.java
@@ -2,9 +2,15 @@ package net.illager.hardcore;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import com.google.gson.JsonParser;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -60,12 +66,31 @@ public class DeathBanLog {
 	}
 
 	/**
+	 * Add a log entry
+	 * @param username The player username
+	 * @param deathBan The death ban to serialize
+	 */
+	public void add(String username, DeathBan deathBan) {
+		UUID uuid = this.fetchUUID(username);
+		this.add(uuid, deathBan);
+	}
+
+	/**
 	 * Remove a log entry
 	 * @param uuid The player UUID
 	 */
 	public void remove(UUID uuid) {
 		this.cache.set(uuid.toString(), null);
 		this.save();
+	}
+
+	/**
+	 * Remove a log entry
+	 * @param username The player username
+	 */
+	public void remove(String username) {
+		UUID uuid = this.fetchUUID(username);
+		this.remove(uuid);
 	}
 
 	/**
@@ -78,10 +103,52 @@ public class DeathBanLog {
 	}
 
 	/**
-	 * Get a log entry
+	 * Checks a log entry existence
+	 * @param username The player username
+	 * @return Whether or not the entry exists
+	 */
+	public boolean has(String username) {
+		UUID uuid = this.fetchUUID(username);
+		return this.has(uuid);
+	}
+
+	/**
+	 * Get a log entry by UUID
 	 * @param uuid The player UUID
 	 */
 	public DeathBan get(UUID uuid) {
 		return (DeathBan) this.cache.get(uuid.toString());
+	}
+
+	/**
+	 * Get a log entry by username
+	 * @param username The player username
+	 */
+	public DeathBan get(String username) {
+		UUID uuid = this.fetchUUID(username);
+		return this.get(uuid);
+	}
+
+	/**
+	 * Fetch a player UUID from the Mojang API
+	 * @param username The player username
+	 */
+	private UUID fetchUUID(String username) {
+		JsonParser parser = new JsonParser();
+		URLConnection request;
+		try {
+			request = new URL("https://api.mojang.com/users/profiles/minecraft/" + username).openConnection();
+			request.connect();
+			InputStreamReader reader = new InputStreamReader((InputStream) request.getContent());
+			String id = parser.parse(reader).getAsJsonObject().get("id").getAsString();
+			String delineatedId = id.replaceFirst(
+				"(\\p{XDigit}{8})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}+)",
+				"$1-$2-$3-$4-$5"
+			);
+			return UUID.fromString(delineatedId);
+		} catch(Exception exception) {
+			this.logger.log(Level.SEVERE, exception.getMessage());
+			return null;
+		}
 	}
 }
--- src/main/java/net/illager/hardcore/ReviveCommand.java
+++ src/main/java/net/illager/hardcore/ReviveCommand.java
@@ -1,18 +1,10 @@
 package net.illager.hardcore;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.UUID;
-import java.util.logging.Level;
-
-import com.google.gson.JsonParser;
-
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class ReviveCommand implements CommandExecutor {
 	private DeathBanLog log;
@@ -25,43 +17,15 @@ public class ReviveCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if(command.getName().equals("revive") && args.length > 0) {
 			for(String username : args) {
-				UUID uuid = fetchUUID(username);
-				if(uuid == null) {
-					sender.sendMessage("Unable to fetch UUID of " + username);
+				if(!this.log.has(username)) {
+					sender.sendMessage(ChatColor.RED + "Count not revive " + username);
 					continue;
 				}
-				if(!this.log.has(uuid)) {
-					sender.sendMessage(username + " is not death banned");
-					continue;
-				}
-				this.log.remove(uuid);
+				this.log.remove(username);
 				sender.sendMessage(username + " was revived");
 			}
 			return true;
 		}
 		return false;
-	}
-
-	/**
-	 * Fetch a player UUID from the Mojang API
-	 * @param username The player username
-	 */
-	private UUID fetchUUID(String username) {
-		JsonParser parser = new JsonParser();
-		URLConnection request;
-		try {
-			request = new URL("https://api.mojang.com/users/profiles/minecraft/" + username).openConnection();
-			request.connect();
-			InputStreamReader reader = new InputStreamReader((InputStream) request.getContent());
-			String id = parser.parse(reader).getAsJsonObject().get("id").getAsString();
-			String delineatedId = id.replaceFirst(
-				"(\\p{XDigit}{8})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}+)",
-				"$1-$2-$3-$4-$5"
-			);
-			return UUID.fromString(delineatedId);
-		} catch(Exception exception) {
-			Bukkit.getLogger().log(Level.SEVERE, exception.getMessage());
-			return null;
-		}
 	}
 }
